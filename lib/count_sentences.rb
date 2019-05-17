e66c92e032eebe5401f9d7b31157ced829287515
johncban <johncban@gmail.com>
2019-05-17 00:27:27 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    
+
   end
 
   def question?
@@ -11,10 +11,14 @@ class String
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
